bd00aacd4cff6e6d3db4b939e25a37270c69a77d
Alex <lacoste.axel87@gmail.com>
2024-11-14 01:13:20 +0100

    logo

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,11 @@
 <template>
     <header class="home-header">
-        <!-- Logo à gauche redirige vers la page d'accueil -->
-        <div class="logo" @mouseover="logoHover = true" @mouseleave="logoHover = false" @click="goHome">
-            <!-- Contenu complet de ton SVG -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="currentColor">
-            <circle cx="20" cy="20" r="18" />
-            <!-- Remplace ici avec les autres éléments de ton SVG original -->
-            </svg>
-        </div>
-  
+      <!-- Importer le logo SVG -->
+      <MyLogo class="logo" @mouseover="logoHover = true" @mouseleave="logoHover = false" />
+    
       <!-- Titre au centre -->
       <h1 class="title">Nuit de l'Info</h1>
-  
+    
       <!-- Menu hamburger à droite -->
       <div class="menu-icon" @click="toggleMenu">
         <div class="line" :class="{ 'open': menuOpen }"></div>
@@ -19,7 +13,7 @@
         <div class="line" :class="{ 'open': menuOpen }"></div>
       </div>
     </header>
-  
+    
     <!-- Menu latéral translucide rouge -->
     <div v-if="menuOpen" class="side-menu" @click.self="toggleMenu">
       <ul>
@@ -32,6 +26,8 @@
   </template>
   
   <script>
+  import MyLogo from '@/assets/images/logo.svg'; // Assurez-vous que le chemin est correct
+  
   export default {
     name: "Header",
     data() {
