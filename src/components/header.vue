d8146fdebf690c1085cb8ed76bd2f6cfa2e02656
Alex <lacoste.axel87@gmail.com>
2024-11-14 02:21:07 +0100

    bug

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,7 @@
 <template>
     <header class="home-header">
-      <!-- Intégration du logo SVG -->
       <div id="logo-container" class="logo"></div>
-  
-      <!-- Titre centré -->
       <h1 class="title">Nuit de l'Info</h1>
-  
-      <!-- Menu hamburger -->
       <div class="menu-icon" @click="toggleMenu">
         <div class="line" :class="{ 'open': menuOpen }"></div>
         <div class="line" :class="{ 'open': menuOpen }"></div>
@@ -14,7 +9,6 @@
       </div>
     </header>
   
-    <!-- Menu latéral -->
     <div v-if="menuOpen" class="side-menu" @click.self="toggleMenu">
       <ul>
         <li @click="toggleMenu">Menu Item 1</li>
@@ -24,28 +18,31 @@
       </ul>
     </div>
   </template>
-<script>
-fetch('@/assets/images/Logo.svg')
+  
+  <script>
+  fetch('@/assets/images/Logo.svg')
     .then(response => response.text())
     .then(svg => {
       document.getElementById('logo-container').innerHTML = svg;
     });
-
-export default {
-  name: "Header",
-  data() {
-    return {
-      menuOpen: false,
-    };
-  },
-  methods: {
-    toggleMenu() {
-      this.menuOpen = !this.menuOpen;
+  
+  export default {
+    name: "Header",
+    data() {
+      return {
+        menuOpen: false,
+      };
     },
-  },
-};
-</script>  
-
-<style>
-  @import './styles/header.css';
+    methods: {
+      toggleMenu() {
+        this.menuOpen = !this.menuOpen;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+    /* Importation du fichier CSS */
+    @import '@/styles/header.css'; /* Correctement ajusté pour le dossier src/styles */
   </style>
+  
