0efb7d908dff2601f3edda362db2e1e586ee109d
Alex <lacoste.axel87@gmail.com>
2024-11-14 01:43:13 +0100

    logooooo

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="home-header">
-      <!-- Importer le logo SVG comme composant Vue -->
-      <Logo class="logo" />
-      
+      <!-- Importer le logo SVG comme image -->
+      <img src="@/assets/images/Logo.svg" alt="Logo" class="logo" />
+  
       <!-- Titre au centre -->
       <h1 class="title">Nuit de l'Info</h1>
-      
+  
       <!-- Menu hamburger à droite -->
       <div class="menu-icon" @click="toggleMenu">
         <div class="line" :class="{ 'open': menuOpen }"></div>
@@ -13,7 +13,7 @@
         <div class="line" :class="{ 'open': menuOpen }"></div>
       </div>
     </header>
-    
+  
     <!-- Menu latéral translucide rouge -->
     <div v-if="menuOpen" class="side-menu" @click.self="toggleMenu">
       <ul>
@@ -26,38 +26,20 @@
   </template>
   
   <script>
-  // Assurez-vous que le chemin est correct pour le fichier SVG
-  import Logo from '@/assets/images/Logo.svg';
-  
   export default {
     name: "Header",
-    components: {
-      Logo
-    },
     data() {
       return {
-        menuOpen: false
+        menuOpen: false,
       };
     },
     methods: {
       toggleMenu() {
         this.menuOpen = !this.menuOpen;
-      }
-    }
+      },
+    },
   };
   </script>
   
-  <style scoped>
-  /* Logo */
-  .logo {
-    width: 50px;  /* Ajustez la taille du logo */
-    height: auto;
-    transition: fill 0.3s ease; /* Transition pour changer la couleur */
-  }
-  
-  /* Change la couleur au survol */
-  .logo:hover {
-    fill: var(--primary-color); /* Couleur du logo au survol */
-  }
-  </style>
+  <style scoped src="../styles/header.css"></style>
   
