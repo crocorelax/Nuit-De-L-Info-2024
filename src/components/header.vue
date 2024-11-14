9099c2da0fddd8e9b16bfd5ad00c2a1693648c4d
Alex <lacoste.axel87@gmail.com>
2024-11-14 01:32:53 +0100

    Update header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="home-header">
       <!-- Importer le logo SVG -->
+      <Logo class="logo" />
     
       <!-- Titre au centre -->
       <h1 class="title">Nuit de l'Info</h1>
