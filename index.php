6f717f5e99a5d18100019347cf7b0a3a0a11ced7
Alex <lacoste.axel87@gmail.com>
2024-12-05 20:43:26 +0100

    +ajout watson et basic

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>index</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
 </head>
 <body>
-    
+    <?php include 'component/header.html'; ?>
+
+    <h1>Bienvenue sur la page d'accueil</h1>
+    <p>Voici le contenu principal.</p>
+
+
+
+    <?php include 'component/footer.html'; ?>
+
 </body>
 </html>
