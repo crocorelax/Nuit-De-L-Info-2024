a82d6c058ffaca0a0a8885860786a072895b4574
Alex <lacoste.axel87@gmail.com>
2024-12-05 19:15:13 +0100

    empty

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; // Assure-toi d'importer correctement le routeur
-
-createApp(App)
-  .use(router) // Utilisation du routeur ici
-  .mount('#app');
